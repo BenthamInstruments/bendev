dffe7956936711c2938531d614e548a1d1b0f377
Markus Führer <markus.fuhrer@bentham.co.uk>
2022-01-10 13:57:16 +0000

    add comment header

--- bendev/__init__.py
+++ bendev/__init__.py
@@ -1,1 +1,6 @@
+# @file __init__.py
+# @author Markus Führer
+# @date 7 Jan 2022
+# @copyright Copyright © 2022 by Bentham Instruments Ltd. 
+
 from bendev.device import Device, list_connected_devices
--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+# @file setup.py
+# @author Markus Führer
+# @date 7 Jan 2022
+# @copyright Copyright © 2022 by Bentham Instruments Ltd. 
+
 import setuptools
 
 with open("readme.md", "r") as fh:
